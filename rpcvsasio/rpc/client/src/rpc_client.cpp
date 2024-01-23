48a3e112b48a3b2ea9a18f6ed8af488c3f4a42fa
TalhaYolcu <y.yolcu2018@gtu.edu.tr>
2024-01-23 16:45:18 +0300

    newly added async grpc

--- rpcvsasio/rpc/client/src/rpc_client.cpp
+++ rpcvsasio/rpc/client/src/rpc_client.cpp
@@ -8,7 +8,18 @@
 #include <ctime>
 #include <sys/time.h>
 #include <cstring>
+#include <unistd.h>
 
+
+using grpc::Channel;
+using grpc::Channel;
+using grpc::ClientAsyncResponseReader;
+using grpc::ClientContext;
+using grpc::CompletionQueue;
+using grpc::Status;
+using expcmake::GetHello;
+using expcmake::query;
+using expcmake::Response;
 
 int main(int argc, char const *argv[])
 {
@@ -41,6 +52,49 @@ int main(int argc, char const *argv[])
     struct timeval start, end;
     gettimeofday(&start, NULL);
 
+    CompletionQueue cq;
+    Status status;
+    Response myResponse;
+    query MyQuery;
+    MyQuery.set_name("Noyan");
+    auto channel = grpc::CreateChannel("localhost:50052", grpc::InsecureChannelCredentials());
+    std::unique_ptr<GetHello::Stub> stub_(GetHello::NewStub(channel));
+
+
+
+    for(int i =0;i<iterations;i++) {
+        
+        ClientContext context;
+        std::unique_ptr<ClientAsyncResponseReader<Response >> rpc(
+        stub_->AsyncGetHelloWorld(&context, MyQuery, &cq));
+
+        // Request that, upon completion of the RPC, "reply" be updated with the
+        // server's response; "status" with the indication of whether the operation
+        // was successful. Tag the request with the integer 1.
+        rpc->Finish(&myResponse, &status, (void*)1);
+        void* got_tag;
+        bool ok = false;
+
+        // Block until the next result is available in the completion queue "cq".
+        // The return value of Next should always be checked. This return value
+        // tells us whether there is any kind of event or the cq_ is shutting down.
+        cq.Next(&got_tag, &ok);
+
+        // Act upon the status of the actual RPC.
+        /*if (status.ok()) {
+            std::cout<<myResponse.message() <<")"<<i<<std::endl;
+        }
+        else {
+            std::cout<<"RPC failed :  "<<status.error_code()<<std::endl;
+        }*/
+    }
+
+ 
+
+
+
+
+/*
     // Call
     auto channel = grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials());
     std::unique_ptr<expcmake::GetHello::Stub> stub = expcmake::GetHello::NewStub(channel);
@@ -49,17 +103,16 @@ int main(int argc, char const *argv[])
     expcmake::query query;
     expcmake::Response result;
 
+/*
     for(int i =0;i<iterations;i++) {
         query.set_name("World");
         grpc::ClientContext context;
 
-        grpc::Status status = stub->GetHelloWorld(&context, query, &result);
+        stub->GetHelloWorld(&context, query, &result);
         // Output result
-        //std::cout << "Message: " << result.message() << std::endl;
-
-
+        //std::cout <<i+1<< ")Message : " << result.message() << std::endl;
     }
-
+*/
     // Get end time
     gettimeofday(&end, NULL);
     long seconds = end.tv_sec - start.tv_sec;
@@ -68,6 +121,11 @@ int main(int argc, char const *argv[])
     std::cout << "Time taken by client: " << elapsed << " seconds" << std::endl;
 
  
+
+
+
+
+
 
     std::cout<<"rpc_client stops..."<<std::endl;
     return 0;
--- rpcvsasio/rpc/server/src/rpc_server.cpp
+++ rpcvsasio/rpc/server/src/rpc_server.cpp
@@ -9,28 +9,27 @@
 #include <csignal>
 #include <cstdlib>
 
+using grpc::Server;
+using grpc::ServerAsyncResponseWriter;
+using grpc::ServerBuilder;
+using grpc::ServerCompletionQueue;
+using grpc::ServerContext;
+using grpc::Status;
+using expcmake::GetHello;
+using expcmake::query;
+using expcmake::Response;
 
 int message_size = 0;
 int iterations = 0;
-bool exit_signal = false;
 
-// Function to handle Ctrl+C signal
-void signalHandler(int signum) {
-    std::cout << "Interrupt signal received (" << signum << "). Exiting...\n";
-
-    if(signum==SIGINT) {
-        exit_signal = true;
-    }
-
-}
-
+/*
 class GetHelloService final : public expcmake::GetHello::Service {
     public:
         virtual ::grpc::Status GetHelloWorld(::grpc::ServerContext* context, const ::expcmake::query* request, ::expcmake::Response* response)
         {        
-            std::string generated = generateHelloWorldString(message_size);
+            //std::string generated = generateHelloWorldString(message_size);
             //std::cout<<"Generated : "<<generated<<std::endl;
-            response->set_message(generated);
+            response->set_message(generateHelloWorldString(message_size));
             return grpc::Status::OK;
         }
         std::string generateHelloWorldString(int length) {
@@ -52,13 +51,162 @@ class GetHelloService final : public expcmake::GetHello::Service {
             return result;
         }
 };
+*/
+class ServerImpl final {
+ public:
+  ~ServerImpl() {
+    server_->Shutdown();
+    // Always shutdown the completion queue after the server.
+    cq_->Shutdown();
+  }
 
+  // There is no shutdown handling in this code.
+  void Run() {
+    
+    grpc::ServerBuilder builder;
+    builder.AddListeningPort("0.0.0.0:50052", grpc::InsecureServerCredentials());
+
+    builder.RegisterService(&service_);
+
+    //std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+    
+    //server->Wait();
+
+    //std::cout<<"rpc_server stops..."<<std::endl;
+    
+    cq_ = builder.AddCompletionQueue();
+    // Finally assemble the server.
+    server_ = builder.BuildAndStart();
+    std::cout << "Server listening on " << "0.0.0.0:50052" << std::endl;
+
+    // Proceed to the server's main loop.
+    HandleRpcs();
+  }
+
+ private:
+  // Class encompasing the state and logic needed to serve a request.
+  class CallData {
+   public:
+    // Take in the "service" instance (in this case representing an asynchronous
+    // server) and the completion queue "cq" used for asynchronous communication
+    // with the gRPC runtime.
+    CallData(GetHello::AsyncService* service, ServerCompletionQueue* cq)
+        : service_(service), cq_(cq), responder_(&ctx_), status_(CREATE) {
+      // Invoke the serving logic right away.
+      Proceed();
+    }
+    int i=0;
+
+    std::string generateHelloWorldString(int length) {
+        const std::string helloWorld = "HelloWorld";
+        std::string result;
+
+        if(i==5) {
+            sleep(2);
+            std::cout<<"AAAA"<<std::endl;
+        }
+        i++;
+
+        if (length <= 0) {
+            std::cerr << "Error: Length should be greater than zero." << std::endl;
+            return result;
+        }
+
+        while (result.length() < length) {
+            result += helloWorld;
+        }
+
+        // Trim the string to the required length
+        result.resize(length);
+
+        return result;
+    }
+
+    void Proceed() {
+      if (status_ == CREATE) {
+        // Make this instance progress to the PROCESS state.
+        status_ = PROCESS;
+
+        // As part of the initial CREATE state, we *request* that the system
+        // start processing SayHello requests. In this request, "this" acts are
+        // the tag uniquely identifying the request (so that different CallData
+        // instances can serve different requests concurrently), in this case
+        // the memory address of this CallData instance.
+        service_->RequestGetHelloWorld(&ctx_, &request_, &responder_, cq_, cq_,
+                                  this);
+      } else if (status_ == PROCESS) {
+        // Spawn a new CallData instance to serve new clients while we process
+        // the one for this CallData. The instance will deallocate itself as
+        // part of its FINISH state.
+        new CallData(service_, cq_);
+
+
+
+        // The actual processing.
+        std::string prefix=generateHelloWorldString(message_size);
+        reply_.set_message(prefix);
+
+        // And we are done! Let the gRPC runtime know we've finished, using the
+        // memory address of this instance as the uniquely identifying tag for
+        // the event.
+        status_ = FINISH;
+        responder_.Finish(reply_, Status::OK, this);
+      } else {
+        GPR_ASSERT(status_ == FINISH);
+        // Once in the FINISH state, deallocate ourselves (CallData).
+        delete this;
+      }
+    }
+
+   private:
+    // The means of communication with the gRPC runtime for an asynchronous
+    // server.
+    GetHello::AsyncService* service_;
+    // The producer-consumer queue where for asynchronous server notifications.
+    ServerCompletionQueue* cq_;
+    // Context for the rpc, allowing to tweak aspects of it such as the use
+    // of compression, authentication, as well as to send metadata back to the
+    // client.
+    ServerContext ctx_;
+
+    // What we get from the client.
+    query request_;
+    // What we send back to the client.
+    Response reply_;
+
+    // The means to get back to the client.
+    ServerAsyncResponseWriter<Response> responder_;
+
+    // Let's implement a tiny state machine with the following states.
+    enum CallStatus { CREATE, PROCESS, FINISH };
+    CallStatus status_;  // The current serving state.
+  };
+
+  // This can be run in multiple threads if needed.
+  void HandleRpcs() {
+    // Spawn a new CallData instance to serve new clients.
+    new CallData(&service_, cq_.get());
+    void* tag;  // uniquely identifies a request.
+    bool ok;
+    while (true) {
+      // Block waiting to read the next event from the completion queue. The
+      // event is uniquely identified by its tag, which in this case is the
+      // memory address of a CallData instance.
+      // The return value of Next should always be checked. This return value
+      // tells us whether there is any kind of event or cq_ is shutting down.
+      GPR_ASSERT(cq_->Next(&tag, &ok));
+      GPR_ASSERT(ok);
+      static_cast<CallData*>(tag)->Proceed();
+    }
+  }
+
+  std::unique_ptr<ServerCompletionQueue> cq_;
+  GetHello::AsyncService service_;
+  std::unique_ptr<Server> server_;
+};
 int main(int argc, char const *argv[])
 {
     std::cout<<"This is rpc_server"<<std::endl;
-
-    // Register signal handler for Ctrl+C
-    std::signal(SIGINT, signalHandler);
 
    // Check if the correct number of arguments is provided
     if (argc != 5) {
@@ -81,7 +229,7 @@ int main(int argc, char const *argv[])
             return 1;
         }
     }
-
+/*
     grpc::ServerBuilder builder;
     builder.AddListeningPort("0.0.0.0:50051", grpc::InsecureServerCredentials());
 
@@ -90,12 +238,13 @@ int main(int argc, char const *argv[])
 
     std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
     
-    while(!exit_signal) {
-        server->Wait();
-    }
+    server->Wait();
 
     std::cout<<"rpc_server stops..."<<std::endl;
+*/
 
+  ServerImpl server;
+  server.Run();
     return 0;
 }
 
