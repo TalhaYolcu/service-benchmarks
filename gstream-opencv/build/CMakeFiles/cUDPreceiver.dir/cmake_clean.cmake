0ff34410a6c9ac06e23a04a47537e3ed7f884330
TalhaYolcu <y.yolcu2018@gtu.edu.tr>
2024-01-24 00:39:22 +0300

    added gstream-opencv

---
+++ gstream-opencv/build/CMakeFiles/cUDPreceiver.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cUDPreceiver.dir/src/receiver.cpp.o"
+  "CMakeFiles/cUDPreceiver.dir/src/receiver.cpp.o.d"
+  "cUDPreceiver"
+  "cUDPreceiver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cUDPreceiver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
